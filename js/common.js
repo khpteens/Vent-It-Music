c315fc9cbdbc7ad2e1feea70ccc663cba0f0a044
Mark Duiker <markergreen@hotmail.com>
2015-08-11 16:24:08 -0400

    broke out GA code

--- js/common.js
+++ js/common.js
@@ -1,7 +1,5 @@
 // Utility.js holds utility functions
 
-var analyticsOn = false; // turn on Google Analytics event tracking 
-
 function trace(s, c, bg) {
 	var style;
 	if (bg === undefined) bg = 'WhiteSmoke';
@@ -148,33 +146,4 @@ function createCopyright() {
 function openInNewTab(url) {
 	var win = window.top.open(url, '_blank');
 	win.focus();
-}
-
-// Google Analytics & Events
-// (function(i, s, o, g, r, a, m) {
-//   i['GoogleAnalyticsObject'] = r;
-//   i[r] = i[r] || function() {
-//     (i[r].q = i[r].q || []).push(arguments)
-//   }, i[r].l = 1 * new Date();
-//   a = s.createElement(o),
-//     m = s.getElementsByTagName(o)[0];
-//   a.async = 1;
-//   a.src = g;
-//   m.parentNode.insertBefore(a, m)
-// })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-// ga('create', 'UA-15452863-1', 'auto');
-// ga('send', 'pageview');
-
-function trackEvent(action, location) {
-
-	location = location.replace("scene", "Scene ");
-
-	var eventCategory = "Game events"; // file all Pic'd tracking events under "Game events" instead of "Interactives"
-
-	var eventAction = gameName + ": " + action; // type of event (Click, etc)
-	var eventLabel = gameName + ": " + location; // property of action (Splash screen, Win screen, etc)
-
-	if (analyticsOn) {
-		ga('send', 'event', eventCategory, eventAction, eventLabel);
-	}
 }
---
+++ js/plugins/google-analytics.js
@@ -0,0 +1,31 @@
+// Google Analytics & Events
+
+var analyticsOn = false; // switch on/off Google Analytics event tracking 
+
+// (function(i, s, o, g, r, a, m) {
+//   i['GoogleAnalyticsObject'] = r;
+//   i[r] = i[r] || function() {
+//     (i[r].q = i[r].q || []).push(arguments)
+//   }, i[r].l = 1 * new Date();
+//   a = s.createElement(o),
+//     m = s.getElementsByTagName(o)[0];
+//   a.async = 1;
+//   a.src = g;
+//   m.parentNode.insertBefore(a, m)
+// })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+// ga('create', 'UA-15452863-1', 'auto');
+// ga('send', 'pageview');
+
+function trackEvent(action, location) {
+
+	location = location.replace("scene", "Scene ");
+
+	var eventCategory = "Game events"; // file all Pic'd tracking events under "Game events" instead of "Interactives"
+
+	var eventAction = gameName + ": " + action; // type of event (Click, etc)
+	var eventLabel = gameName + ": " + location; // property of action (Splash screen, Win screen, etc)
+
+	if (analyticsOn) {
+		ga('send', 'event', eventCategory, eventAction, eventLabel);
+	}
+}
